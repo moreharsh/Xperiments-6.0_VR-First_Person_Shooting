f0cfeb047f71129dddc3cb034c78f725ebfa4b69
moreharsh <moreharsh2001@gmail.com>
2022-11-24 09:54:41 +0530

    Unity Files Added

---
+++ Assets/Main_Menu/Menu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void StartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+    
+}
